f445a4358672e7a07ef6d7952bac3455433857a5
Austin Melms <memyselfandlie@gmail.com>
2018-06-01 01:56:02 -0400

    init

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+app.use('/api/discord', require('./api/discord'))
+
+app.get('/', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.listen(50451, () => {
+  console.info('Running on port 50451')
+})
+
+app.use((err, req, res, next) => {
+  switch (err.message) {
+    case 'NoCodeProvided':
+      return res.status(400).send({
+        status: 'ERROR',
+        error: err.message
+      })
+    default:
+      return res.status(500).send({
+        status: 'ERROR',
+        error: err.message
+      })
+  }
+})
